406a6b42643558b2e1aec5355d2c4c08ebe8667b
arunrajvijayan <arunrajvijayan007@gmail.com>
2020-08-27 15:38:28 +0530

    application commited

---
+++ application/helpers/common_helper.php
@@ -0,0 +1,11 @@
+<?php
+function generateInnvoice() {
+    // $otp = rand(100000, 999999);
+    // return $otp;
+    $num=913207;
+
+    $res=$num."01";
+    $innovice_code = 'SH/US/'.$res;
+        return $innovice_code;
+}
+?>
